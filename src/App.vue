0134a646b1b4f2682e775b5d866ffea03b6306e1
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-26 14:34:18 +0100

    basic printing of objects taken from external api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <RecordList/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import RecordList from './components/RecordList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RecordList
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
---
+++ src/components/RecordList.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <RecordInfo
+    v-for="record, i in recordList"
+    :key="i"
+    :recordInfos="record"
+    />
+</div>
+
+
+</template>
+
+<script>
+import axios from "axios";
+import RecordInfo from './RecordInfo.vue'
+
+export default {
+  name: 'RecordList',
+  components: {
+    RecordInfo
+  },
+  data() {
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+      recordList: []
+    }
+  },
+  created () {
+    this.getRecords();
+  },
+  methods: {
+      getRecords() {
+        axios
+        .get(this.apiUrl)
+        .then((foundList) => {
+          this.recordList = foundList.data.response
+          console.log(foundList.data.response);
+        })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+
+</style>
