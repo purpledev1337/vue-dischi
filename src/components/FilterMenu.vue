e798246f8f941286e5e8d3c6200a1ef37ea4832a
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-30 00:57:27 +0100

    filter w function linked to input v-mod+auto genre

---
+++ src/components/FilterMenu.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <!-- send to the parent the selected option value -->
+    <select @change="$emit('filter', selected)"
+        v-model="selected"
+        name="type_list">
+            <option
+            disabled
+            value="">
+                Select music genre
+            </option>
+            <option
+            value="all">
+                All genres
+            </option>
+            <!-- printing of all genres found -->
+            <option
+            v-for="genre, i in recordGenres"
+            :key="i"
+            :value="i">
+                {{ genre }}
+            </option>
+
+    </select>
+
+</template>
+
+<script>
+export default {
+  name: 'FilterMenu',
+//   genre list taken from parent
+  props: {
+      recordGenres: Array
+  },
+  data() {
+      return {
+          selected: null
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+select {
+    width: 200px;
+    transform: scale(1.5);
+}
+
+</style>
--- src/components/RecordList.vue
+++ src/components/RecordList.vue
@@ -2,15 +2,25 @@
 
   <div id="record_list_container">
 
+    <nav>
+      <!-- sending the genre list array to the child -->
+      <!-- + filter selection function -->
+      <FilterMenu
+      :recordGenres="recordGenreList"
+      @filter="selectFilter"
+      />
+    </nav>
+
     <div id="record_card_container"
       v-if="recordList.length === recordListLength"
       >
-
+      <!-- print cards from already filtered list -->
         <RecordCard
-        v-for="record, i in recordList"
+        v-for="record, i in filteredRecordList"
         :key="i"
         :recordInfos="record"
         />
+
     </div>
 
     <div id="loading_screen" v-else></div>
@@ -22,21 +32,39 @@
 <script>
 import axios from "axios";
 import RecordCard from './RecordCard.vue'
+import FilterMenu from './FilterMenu.vue'
+
 
 export default {
   name: 'RecordList',
   components: {
-    RecordCard
+    RecordCard,
+    FilterMenu
   },
   data() {
     return {
       apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
       recordList: [],
-      recordListLength : null
+      recordListLength : null,
+      recordGenreList : [],
+      selectedFilter: null
     }
   },
   created () {
+    // demo intent timeout
     setTimeout(() => this.getRecords(), 1000);
+  },
+  computed: {
+    // computed variable to maintain original array data
+    filteredRecordList() {
+      if (this.selectedFilter === null) {
+        return this.recordList
+      }
+      return this.recordList.filter((element) => {
+        return element.genre.includes(this.selectedFilter)
+      })
+
+    }
   },
   methods: {
       getRecords() {
@@ -45,7 +73,24 @@ export default {
         .then((foundList) => {
           this.recordList = foundList.data.response;
           this.recordListLength = foundList.data.response.length;
-          console.log(foundList.data.response.length)})
+          console.log(foundList.data.response.length);
+          // for loop to generate genre list automatically after api call
+          for (let i = 0; i < foundList.data.response.length; i++ ){
+            if (!this.recordGenreList.includes(foundList.data.response[i].genre)) {
+              this.recordGenreList.push(foundList.data.response[i].genre)
+            }
+          }
+          console.log(this.recordGenreList);
+          })
+      },
+      // value received from child will be the key number of the genre list 
+      selectFilter(filteredGenre) {
+        if (filteredGenre === "all") {
+          this.selectedFilter = null
+        }
+        else {
+          this.selectedFilter = this.recordGenreList[filteredGenre]
+        }
       }
   }
 }
@@ -60,10 +105,17 @@ export default {
   min-height: calc(100vh - 70px);
   margin: auto;
 
+  nav {
+    height: 80px;
+    text-align: center;
+    line-height: 80px;
+  }
+
   #record_card_container {
     display: flex;
     flex-wrap: wrap;
-    padding: 60px 0;
+    padding-bottom: 50px;
+    padding-top: 20px;
   }
 
   #loading_screen {
